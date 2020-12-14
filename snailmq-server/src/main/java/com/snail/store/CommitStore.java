fb851364c766a320e67dd9cca43c304eafdf8914
a1215305395 <38575998+a1215305395@users.noreply.github.com>
2020-12-14 16:46:17 +0800

    增加基础CommitStore实现类

---
+++ snailmq-server/src/main/java/com/snail/store/CommitStore.java
@@ -0,0 +1,12 @@
+package com.snail.store;
+
+/**
+ * @version V1.0
+ * @author: csz
+ * @Title
+ * @Package: com.snail.store
+ * @Description:
+ * @date: 2020/12/14
+ */
+public class CommitStore {
+}
